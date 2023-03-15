e1978d8316ec8dc720cd23c758a94464a768a193
Helena <v0robjeva.alena@yandex.ru>
2023-03-15 21:30:46 +0300

    API/CI11

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -9,19 +9,15 @@ import static org.hamcrest.Matchers.is;
 class MobileBankApiTestV4 {
     @Test
     void shouldReturnDemoAccounts() {
-      /*
-       Given - When - Then
-       Предусловия
-      */
         given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-      ;
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
     }
 }
